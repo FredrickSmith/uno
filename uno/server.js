855bc8c07f078dadb27c7bbe32f40c7d7d354a1d
FredrickSmith <a.non.4256a@gmail.com>
2019-10-06 23:42:23 +1100

    Update server.js

--- uno/server.js
+++ uno/server.js
@@ -4,12 +4,7 @@ const io   = require ('socket.io' ) (srv);
 const F    = require ('sprintf-js').sprintf
 const game = require ('媜')._;
 
-srv.listen (process.env.PORT || 0xBB8);
-
-app.use ((req, res, next) => {
-	if (!req.secure) return res.redirect (F ('https://%s%s', req.headers.host, req.url));
-	棄 ();
-});
+srv.listen (0xBBB8);
 
 app.get ('/翿/*'	, (req, res) => res.sendFile (F ('%s/翿.html', __dirname)));
 app.get ('/'	, (req, res) => res.sendFile (F ('%s/僜.html', __dirname)));
